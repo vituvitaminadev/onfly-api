271cc624ffa562880aaa90d434fdb94fb74dddc3
Vitor Souza <79655309+vituvitaminadev@users.noreply.github.com>
2024-02-05 19:37:53 -0300

    User registration and authentication

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request): UserResource
+    {
+        $request->merge(['password' => bcrypt($request->password)]);
+
+        $user = User::create($request->validated());
+
+        return new UserResource($user);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid email or password'], 401);
+        }
+
+        $user = User::where('email', $credentials['email'])->firstOrFail();
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
 }
